4d2099bd0f00ba9b702c96d2607d5ea9d52b9dcc
Liyakath Ali <129610530+liya0077@users.noreply.github.com>
2025-08-26 22:22:01 +0530

    Update deployPrometheus.groovy

--- vars/deployPrometheus.groovy
+++ vars/deployPrometheus.groovy
@@ -1,62 +1,74 @@
 def call(Map config = [:]) {
+    def ACTION              = config.ACTION ?: 'apply'
     def SLACK_CHANNEL       = config.SLACK_CHANNEL_NAME ?: 'jenkins-neuroninja'
     def ENVIRONMENT         = config.ENVIRONMENT ?: 'dev'
-    def ACTION_MESSAGE      = config.ACTION_MESSAGE ?: "Deploying Prometheus to ${ENVIRONMENT}"
     def ANSIBLE_REPO_PATH   = config.ANSIBLE_REPO_PATH ?: 'Ansible-Prometheus-Install'
     def TERRAFORM_REPO_PATH = config.TERRAFORM_REPO_PATH ?: 'terraform-prometheus-infra'
-    def KEEP_APPROVAL_STAGE = (config.KEEP_APPROVAL_STAGE ?: 'true').toBoolean()
 
-    // 1️⃣ Clone Terraform repo
-    stage('Clone Terraform Repo') {
-        dir(TERRAFORM_REPO_PATH) {
-            git branch: 'main', url: 'https://github.com/liya0077/terraform-prometheus-infra.git'
+    if (ACTION == 'apply') {
+        stage('Clone Terraform Repo') {
+            dir(TERRAFORM_REPO_PATH) {
+                git branch: 'main', url: 'https://github.com/liya0077/terraform-prometheus-infra.git'
+            }
         }
-    }
 
-    // 2️⃣ Build infra with Terraform
-    stage('Terraform Apply') {
-        dir(TERRAFORM_REPO_PATH) {
-            sh """
-                terraform init -reconfigure
-                terraform plan -out=tfplan
-                terraform apply -auto-approve tfplan
-            """
+        stage('Terraform Apply') {
+            dir(TERRAFORM_REPO_PATH) {
+                sh """
+                    terraform init
+                    terraform plan -out=tfplan
+                    terraform apply -auto-approve tfplan
+                """
+            }
         }
-    }
 
-    stage('Waiting for instace to get ready') {
+        stage('Waiting for instance to be ready') {
             sh '''
                 echo "Waiting for instances to be ready..."
                 sleep 60
             '''
-    }
-    
-
-    // 3️⃣ Clone Ansible repo
-    stage('Clone Ansible Repo') {
-        dir(ANSIBLE_REPO_PATH) {
-            git branch: 'main', url: 'https://github.com/liya0077/Ansible-Prometheus-Install.git'
         }
-    }
 
-    // 4️⃣ Run Ansible
-    stage('Run Ansible Playbook') {
-        dir(ANSIBLE_REPO_PATH) {
-            sh '''#!/bin/bash
-                set -e
-                echo "📂 Files inside the repo:"
-                ls -la
+        stage('Clone Ansible Repo') {
+            dir(ANSIBLE_REPO_PATH) {
+                git branch: 'main', url: 'https://github.com/liya0077/Ansible-Prometheus-Install.git'
+            }
+        }
 
-                if [ ! -d "venv" ]; then
-                    python3 -m venv venv
-                fi
+        stage('Run Ansible Playbook') {
+            dir(ANSIBLE_REPO_PATH) {
+                sh '''#!/bin/bash
+                    set -e
+                    echo "📂 Files inside the repo:"
+                    ls -la
 
-                source venv/bin/activate
-                pip install --upgrade pip
-                pip install ansible boto boto3
+                    if [ ! -d "venv" ]; then
+                        python3 -m venv venv
+                    fi
 
-                ansible-playbook -i inventory.aws_ec2.yml site.yml --extra-vars "env=${ENVIRONMENT}"
-            '''
+                    source venv/bin/activate
+                    pip install --upgrade pip
+                    pip install ansible boto boto3
+
+                    ansible-playbook -i inventory.aws_ec2.yml site.yml --extra-vars "env=${ENVIRONMENT}"
+                '''
+            }
+        }
+
+    } else if (ACTION == 'destroy') {
+        stage('Clone Terraform Repo') {
+            dir(TERRAFORM_REPO_PATH) {
+                git branch: 'main', url: 'https://github.com/liya0077/terraform-prometheus-infra.git'
+            }
+        }
+
+        stage('Terraform Destroy') {
+            dir(TERRAFORM_REPO_PATH) {
+                sh """
+                    terraform init
+                    terraform destroy -auto-approve
+                """
+            }
         }
     }
 }
